9e433a6c18bd3e225d0c35623db409a157f58b17
b99858 <mark.jones@torontopolice.on.ca>
2021-09-24 09:38:46 -0400

    Added to 2021: - Aftermath: Life Debt

--- 2021/web_page_version/script.js
+++ 2021/web_page_version/script.js
@@ -2,145 +2,199 @@ var books = [{
     "title": "Tarkin",
     "author": "James Luceno",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/81AwzEFa+dL.jpg",
-    "pages": 354
-},
-{
-    "title": "Dark Lord",
+    "pages": 354,
+  "timeline": "14 BBY",
+  "era": "Reign of the Empire"
+}, {
+    "title": "Dark Lord: The Rise of Darth Vader",
     "author": "James Luceno",
     "cover": "https://d188rgcu4zozwl.cloudfront.net/content/B00513HXAG/resources/1713329629",
-    "pages": 369
+    "pages": 369,
+  "timeline": "19 BBY",
+  "era": "Reign of the Empire"
 }, {
     "title": "Revan",
     "author": "Drew Karpyshyn",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/81pVtrJRfhL.jpg",
-    "pages": 368
+    "pages": 368,
+    "timeline": "3950 BBY",
+    "era": "Old Republic"
 }, {
     "title": "Darth Bane: Path of Destruction",
     "author": "Drew Karpyshyn",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/919IhaKl1qL.jpg",
-    "pages": 324
+    "pages": 324,
+    "timeline": "1003 BBY - 1000 BBY",
+    "era": "Old Republic"
 }, {
     "title": "Darth Bane: Rule of Two",
     "author": "Drew Karpyshyn",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/91Nsu5lwOkL.jpg",
-    "pages": 352
+    "pages": 352,
+    "timeline": "1000 BBY - 990 BBY",
+    "era": "Old Republic"
 }, {
     "title": "Darth Bane: Dynasty of Evil",
     "author": "Drew Karpyshyn",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/91hCdXC3yIL.jpg",
-    "pages": 336
+    "pages": 336,
+    "timeline": "980 BBY",
+    "era": "Old Republic"
 }, {
     "title": "Heir to the Empire",
     "author": "Timothy Zahn",
     "cover": "https://d188rgcu4zozwl.cloudfront.net/content/B00513HX7Y/resources/366133765",
-    "pages": 416
+    "pages": 416,
+    "timeline": "9 ABY",
+    "era": "New Republic"
 },{
     "title": "Dark Force Rising",
     "author": "Timothy Zahn",
     "cover": "https://d188rgcu4zozwl.cloudfront.net/content/B00513HKF4/resources/1863583663",
-    "pages": 450
+    "pages": 450,
+    "timeline": "9 ABY",
+    "era": "New Republic"
 },{
     "title": "The Last Command",
     "author": "Timothy Zahn",
     "cover": "https://d188rgcu4zozwl.cloudfront.net/content/B00513HJXC/images/cover.jpg",
-    "pages": 595
+    "pages": 595,
+    "timeline": "9 ABY",
+    "era": "New Republic"
 },{
     "title": "Resistance Reborn",
     "author": "Rebecca Roanhorse",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/91zJfaEwrxL.jpg",
-    "pages": 432
+    "pages": 432,
+    "timeline": "34 ABY",
+    "era": "Rise of the First Order"
 },{
     "title": "Thrawn",
     "author": "Timothy Zahn",
     "cover": "https://d188rgcu4zozwl.cloudfront.net/content/B01ILZO30S/images/cover.jpg",
-    "pages": 488
+    "pages": 488,
+    "timeline": "11 BBY - 2 BBY",
+    "era": "Age of Rebellion"
 },{
     "title": "Thrawn: Alliances",
     "author": "Timothy Zahn",
     "cover": "https://d374oxlv7wyffd.cloudfront.net/B07692QXW3/f83a49ce/cover.jpeg",
-    "pages": 483
+    "pages": 483,
+    "timeline": "19 BBY - 2 BBY",
+    "era": "Age of Rebellion"
 },
  {
     "title": "Thrawn: Treason",
     "author": "Timothy Zahn",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/81vnLArJsuL.jpg",
-    "pages": 496
+    "pages": 496,
+    "timeline": "1 BBY",
+    "era": "Age of Rebellion"
 },
  {
     "title": "Allegiance",
     "author": "Timothy Zahn",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/91UYdSUnpFL.jpg",
-    "pages": 336
+    "pages": 336,
+    "timeline": "0 ABY",
+    "era": "Rebellion"
 },
  {
     "title": "Choices of One",
     "author": "Timothy Zahn",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/81cIJEItXcL.jpg",
-    "pages":480
+    "pages":480,
+    "timeline": "0 ABY",
+    "era": "Rebellion"
 },
  {
     "title": "I, Jedi",
     "author": "Michael A. Stackpole",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/717jFriRA8L.jpg",
-    "pages":592
+    "pages":592,
+    "timeline": "11 ABY",
+    "era": "New Republic"
 },
  {
     "title": "Lost Stars",
     "author": "Claudia Gray",
     "cover": "https://static.wikia.nocookie.net/starwars/images/2/25/Lost_Stars.png",
-    "pages":576
+    "pages":576,
+    "timeline": "11 BBY - 5 ABY",
+    "era": "New Republic"
 },
  {
     "title": "Deceived",
     "author": "Paul S. Kemp",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/919S9OE8SSL.jpg",
-    "pages":352
+    "pages":352,
+    "timeline": "3653 BBY ",
+    "era": "Old Republic"
 },
  {
     "title": "Fatal Alliance",
     "author": "Sean Williams",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/81JHIPuZ96L.jpg",
-    "pages":512
+    "pages":512,
+    "timeline": "3643 BBY",
+    "era": "Old Republic"
 },
  {
     "title": "Dawn of the Jedi: Into the Void",
     "author": "Tim Lebbon",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/916mJi0SzML.jpg",
-    "pages":352
+    "pages":352,
+    "timeline": "25,802 BBY - 25,793 BBY",
+    "era": "Old Republic"
 },
  {
     "title": "Lost Tribe of the Sith: The Collected Stories",
     "author": "John Jackson Miller",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/91OngTdLjpL.jpg",
-    "pages":432
+    "pages":432,
+    "timeline": "5000 BBY- 2975 BBY",
+    "era": "Old Republic"
 },
-{
+ {
     "title": "Lords of the Sith",
     "author": "Paul S. Kemp",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/91bC9qa3yVL.jpg",
-    "pages":368
+    "pages":368,
+    "timeline": "14 BBY",
+    "era": "Reign of the Empire"
 },
  {
     "title": "Dark Disciple",
     "author": "Christie Golden",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/914jILPpk8L.jpg",
-    "pages":400
-}, {
+    "pages":400,
+    "timeline": "19 BBY",
+    "era": "Fall of the Empire"
+},
+ {
     "title": "Outbound Flight",
     "author": "Timothy Zahn",
     "cover": "https://d374oxlv7wyffd.cloudfront.net/B00513F9OS/74698d8a/cover.jpeg",
-    "pages":420
+    "pages":420,
+    "timeline": "27 BBY",
+    "era": "Rise of the Sith"
 },
  {
     "title": "Aftermath",
     "author": "Chuck Wendig",
     "cover": "https://images-na.ssl-images-amazon.com/images/I/71A0u-M6MdL.jpg",
-    "pages":410
+    "pages":410,
+    "timeline": "4 ABY",
+    "era": "New Republic"
+},
+ {
+    "title": "Aftermath: Life Debt",
+    "author": "Chuck Wendig",
+    "cover": "https://images-na.ssl-images-amazon.com/images/I/71T-Qa6gKgL.jpg",
+    "pages":480,
+    "timeline": "5 ABY",
+    "era": "New Republic"
 }
-
-
-
-
         ]
 
 const mainContent = document.getElementById("book-content");
