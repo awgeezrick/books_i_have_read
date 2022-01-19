5bcc66d2db2c5450e295fba7b97ebc3c73b03f9e
b99858 <mark.jones@torontopolice.on.ca>
2022-01-19 09:17:58 -0500

    - created 2022 - added Darth Plagueis - changed the storyline, era, timeline stuff since it doesn't apply to the books for this year. However, I did like being able to filter stuff so I changed era and storyline to 'main_genre' and 'sub_genre'

---
+++ 2022/web_page_version/script.js
@@ -0,0 +1,44 @@
+var books = [{
+   "title": "Darth Plagueis",
+    "author": "James Luceno",
+    "cover": "https://images-na.ssl-images-amazon.com/images/I/91Ks5hTuBeL.jpg",
+    "pages": 498,
+  "main_genre": "Fiction",
+  "sub_genre": "Science Fiction"
+}
+        ]
+
+const mainContent = document.getElementById("book-content");
+
+showBooks(books);
+
+function showBooks(books) {
+  mainContent.innerHTML = "";
+  books.forEach((book) => {
+    const bookTitle = book.title;
+    const bookCover = book.cover;
+    //const bookVote = book.vote_average;
+    const bookElm = document.createElement("div");
+    bookElm.classList.add(
+      "col-xs-12",
+      "col-sm-6",
+      "col-md-4",
+      "col-lg-4",
+      "p-0"
+    );
+    bookElm.innerHTML = `
+    <div class="book-card">
+              <img
+                class="img-fluid book-img"
+                src="${bookCover}" />
+              <div
+                class="book-description p-3 d-flex justify-content-between align-items-center"
+              >
+                <h3 class="book-title">${bookTitle}</h3>
+                
+              </div>
+            </div>
+    `;
+    mainContent.appendChild(bookElm);
+  });
+}
