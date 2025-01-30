a0b49a07510b93a0fe31c87dce765281ceb39965
awgeezrick <mark.jones@torontopolice.on.ca>
2025-01-30 13:40:40 -0500

    - 2025 - Added: A New Dawn, Armada, Vector Prime

---
+++ 2025/web_page_version/script.js
@@ -0,0 +1,61 @@
+var books = [{
+   "title": "A New Dawn",
+    "author": "John Jackson Miller",
+    "cover": "https://m.media-amazon.com/images/I/71a8lzJpiLL._SL1200_.jpg",
+    "pages": 416,
+  "main_genre": "Fiction",
+  "sub_genre": "Science Fiction"
+},
+{
+   "title": "Armada",
+    "author": "Ernest Cline",
+    "cover": "https://m.media-amazon.com/images/I/61fuQ7f70UL._SL1200_.jpg",
+    "pages": 368,
+  "main_genre": "Fiction",
+  "sub_genre": "Science Fiction"
+},
+{
+   "title": "Vector Prime",
+    "author": "R.A. Salvatore",
+    "cover": "https://m.media-amazon.com/images/I/71xqf9pwygL._SL1200_.jpg",
+    "pages": 416,
+  "main_genre": "Fiction",
+  "sub_genre": "Science Fiction"
+}
+]
+
+
+const mainContent = document.getElementById("book-content");
+
+showBooks(books);
+
+function showBooks(books) {
+  mainContent.innerHTML = "";
+  books.forEach((book) => {
+    const bookTitle = book.title;
+    const bookCover = book.cover;
+    //const bookVote = book.vote_average;
+    const bookElm = document.createElement("div");
+    bookElm.classList.add(
+      "col-xs-12",
+      "col-sm-6",
+      "col-md-4",
+      "col-lg-4",
+      "p-0"
+    );
+    bookElm.innerHTML = `
+    <div class="book-card">
+              <img
+                class="img-fluid book-img"
+                src="${bookCover}" />
+              <div
+                class="book-description p-3 d-flex justify-content-between align-items-center"
+              >
+                <h3 class="book-title">${bookTitle}</h3>
+                
+              </div>
+            </div>
+    `;
+    mainContent.appendChild(bookElm);
+  });
+}
