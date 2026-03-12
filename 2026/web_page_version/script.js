83304ed620a4964de7633fd0ee845a49f612249d
Mark Jones <40799959+awgeezrick@users.noreply.github.com>
2026-03-12 16:31:31 -0400

    Updated additional books read
    Added:
    - The Waste Lands
    - Wizard and Glass
    - Wolves of the Calla
    - Song of Susannah
    - The Dark Tower
    - The Wind Through the Keyhole: A Dark Tower Novel

--- 2026/web_page_version/script.js
+++ 2026/web_page_version/script.js
@@ -1,17 +1,66 @@
 var books = [{
-   "title": "The Gunslinger",
-    "author": "Stephen King",
-    "cover": "https://m.media-amazon.com/images/I/71LiCxdD-ZL._SL1500_.jpg",
-    "pages": 288,
-  "main_genre": "Fiction",
-  "sub_genre": "Fantasy"
-}, {
-   "title": "The Drawing of the Three",
-    "author": "Stephen King",
-    "cover": "https://m.media-amazon.com/images/I/71KQ324N9vL._SL1500_.jpg",
-    "pages": 496,
-  "main_genre": "Fiction",
-  "sub_genre": "Fantasy"
+  "title": "The Gunslinger",
+   "author": "Stephen King",
+   "cover": "https://m.media-amazon.com/images/I/71LiCxdD-ZL._SL1500_.jpg",
+   "pages": 288,
+ "main_genre": "Fiction",
+ "sub_genre": "Fantasy"
+},
+ {
+  "title": "The Drawing of the Three",
+   "author": "Stephen King",
+   "cover": "https://m.media-amazon.com/images/I/71KQ324N9vL._SL1500_.jpg",
+   "pages": 496,
+ "main_genre": "Fiction",
+ "sub_genre": "Fantasy"
+},
+{
+"title": "The Waste Lands",
+ "author": "Stephen King",
+ "cover": "https://m.media-amazon.com/images/I/81kcyaNSbdL._SL1500_.jpg",
+ "pages": 640,
+"main_genre": "Fiction",
+"sub_genre": "Fantasy"
+},
+{
+"title": "Wizard and Glass",
+ "author": "Stephen King",
+ "cover": "https://m.media-amazon.com/images/I/81eZ7xI608L._SL1500_.jpg",
+ "pages": 928,
+"main_genre": "Fiction",
+"sub_genre": "Fantasy"
+},
+{
+"title": "Wolves of the Calla",
+ "author": "Stephen King",
+ "cover": "https://m.media-amazon.com/images/I/71dOKrO5DGL._SL1500_.jpg",
+ "pages": 736,
+"main_genre": "Fiction",
+"sub_genre": "Fantasy"
+},
+{
+"title": "Song of Susannah",
+ "author": "Stephen King",
+ "cover": "https://m.media-amazon.com/images/I/71ZDE1KvEtL._SL1500_.jpg",
+ "pages": 448,
+"main_genre": "Fiction",
+"sub_genre": "Fantasy"
+},
+{
+"title": "The Dark Tower",
+ "author": "Stephen King",
+ "cover": "https://m.media-amazon.com/images/I/71rxkomEqdL._SL1500_.jpg",
+ "pages": 864,
+"main_genre": "Fiction",
+"sub_genre": "Fantasy"
+},
+{
+"title": "The Wind Through the Keyhole: A Dark Tower Novel",
+ "author": "Stephen King",
+ "cover": "https://m.media-amazon.com/images/I/81tUzi16hQL._SL1500_.jpg",
+ "pages": 320,
+"main_genre": "Fiction",
+"sub_genre": "Fantasy"
 }
 ]
 
